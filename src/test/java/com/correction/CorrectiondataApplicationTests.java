60da3d493c515877cefef634301c2832b203ac80
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-31 09:42:45 +0100

    Initial commit

---
+++ src/test/java/com/correction/CorrectiondataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrectiondataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
